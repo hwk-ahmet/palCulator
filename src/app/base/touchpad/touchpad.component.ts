ebfe141dab4d49770a6dab9b8c1ca424ae765e94
Ahmet Atmaca <handsomerainman@gmail.com>
2021-05-12 01:23:35 +0200

    font , styling

---
+++ src/app/base/touchpad/touchpad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-touchpad',
+  templateUrl: './touchpad.component.html',
+  styleUrls: ['./touchpad.component.css']
+})
+export class TouchpadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
